dea632cf84a68885d589b24fca45f7a730690dd8
Todicamer <31813467+Todicamer@users.noreply.github.com>
2017-09-10 11:19:12 -0400

    numero 2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var url = require("url");
 var fs = require("fs");
 var express = require("express");
 
+//para que soporte openshift
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
